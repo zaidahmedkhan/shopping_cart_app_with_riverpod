83ec51d0b917b3ad15f01d2fe56f1eecf38734e1
John Doe <zaidkhan200415@gmail.com>
2025-01-07 18:29:10 +0500

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_files/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
